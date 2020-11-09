a88ffaf4ac5ad3736d9a937395e29269c2f0dc34
gustavjlindqvist <gurra.lindqvist@gmail.com>
2020-11-09 16:37:38 +0100

    visionCone v1

---
+++ Assets/Scripts/light.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class light : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.z, 10);
+        //Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+        //lookPos = lookPos - transform.position;
+        //Get the Screen positions of the object
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint (transform.position);
+         
+        //Get the Screen position of the mouse
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        
+        //Get the angle between the points
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+ 
+        //Ta Daaa
+        transform.rotation = Quaternion.Euler (new Vector3(0f,0f,angle));
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+}
