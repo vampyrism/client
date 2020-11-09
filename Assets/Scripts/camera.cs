9be039fbc7a2860862bfbe70aeb59ec80af7cc6c
David Östlund <d.andersson.1994@gmail.com>
2020-11-09 10:49:40 +0100

    Start client project

---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    [Header("Camera Options")]
+    [SerializeField]
+    protected Transform trackingTarget;
+    private Vector3 offset;
+
+    [Tooltip("Speed at which camera follows (float)")]
+    public float followSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - trackingTarget.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position =
+            new Vector3
+            (
+                Mathf.Lerp(transform.position.x, trackingTarget.position.x, Time.fixedDeltaTime * followSpeed),
+                Mathf.Lerp(transform.position.y, trackingTarget.position.y, Time.fixedDeltaTime * followSpeed),
+                -20
+            );
+    }
+}
---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float moveLimiter = 0.7f;
+
+    public float runSpeed = 0.5f;
+    public int MinDistance = 1;
+
+    private Transform target;
+    private Animator animator;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        animator  = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag ("Player").transform;
+    }
+
+    void Update()
+    {
+        transform.right = target.position - transform.position;
+
+        //move if distance from target is greater than MinDist
+        if (Vector3.Distance(transform.position, target.position) > MinDistance){
+
+            transform.position = Vector2.MoveTowards(transform.position, target.position, runSpeed * Time.deltaTime);
+        } else {
+            animator.SetTrigger("enemyAttack");
+        }
+    }
+}
