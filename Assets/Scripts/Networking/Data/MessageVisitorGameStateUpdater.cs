0b9b3c2f41206a689bf094b4128e890d15cfa738
David Östlund <d.andersson.1994@gmail.com>
2021-01-05 15:55:26 +0100

    Fixed some visual bugs
    Top left health bar not connected to correct player
    
    Weapon selection not showing correctly (probably synced to all or wrong player)
    
    Player sword attack slightly off
    
    Going through bushes
    
    Hit animation sometimes off
    
    Enemy animations not always the correct direction

--- Assets/Scripts/Networking/Data/MessageVisitorGameStateUpdater.cs
+++ Assets/Scripts/Networking/Data/MessageVisitorGameStateUpdater.cs
@@ -136,6 +136,7 @@ namespace Assets.Server
                     GameManager.instance.Entities.TryGetValue((UInt32)m.GetEntityID(), out Entity e);
                     Player p = (Player)e;
                     p.Controllable = true;
+                    p.SetupControllable();
                     GameManager.instance.currentPlayer = p;
 
                     p.gameObject.AddComponent<InputHandler>();
